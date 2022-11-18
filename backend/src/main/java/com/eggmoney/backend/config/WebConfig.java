870722afe0a75f57db639997593ba5710aa85923
YOUNG <moxnox63@gmail.com>
2022-11-18 11:05:09 +0900

    diary xml

--- backend/src/main/java/com/eggmoney/backend/config/WebConfig.java
+++ backend/src/main/java/com/eggmoney/backend/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.eggmoney.backend.interceptor.JwtInterceptor;
+//import com.eggmoney.backend.interceptor.JwtInterceptor;
 
 @Configuration
 @EnableWebMvc
@@ -26,18 +26,18 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     // 아래는 JwtInterceptor 등록
-    @Autowired
-    private JwtInterceptor jwtInterceptor;
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(jwtInterceptor)
-                .addPathPatterns("/**") // 들어오는 모든 주소를 intercept할거야
-                .excludePathPatterns("api/login",  // 로그인 요청으로 들어오면 봐줘!
-                        // 그 외에 swagger 페이지 확인하려면 아래의 path도 좀 봐줘!
-                        "/swagger-resource/**",
-                        "/swagger-ui/**",
-                        "/v2/api-docs");
-    }
+//    @Autowired
+//    private JwtInterceptor jwtInterceptor;
+//
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(jwtInterceptor)
+//                .addPathPatterns("/**") // 들어오는 모든 주소를 intercept할거야
+//                .excludePathPatterns("api/login",  // 로그인 요청으로 들어오면 봐줘!
+//                        // 그 외에 swagger 페이지 확인하려면 아래의 path도 좀 봐줘!
+//                        "/swagger-resource/**",
+//                        "/swagger-ui/**",
+//                        "/v2/api-docs");
+//    }
 
 }
--- backend/src/main/java/com/eggmoney/backend/controller/DiaryRestController.java
+++ backend/src/main/java/com/eggmoney/backend/controller/DiaryRestController.java
@@ -45,5 +45,4 @@ public class DiaryRestController {
         diaryService.modifyComment(comment);
         return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
     }
-
 }
