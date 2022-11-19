a1299b2d8031c94b2d390ce9e8677bac2af5981a
hiflo <xswaqz@naver.com>
2022-11-19 23:11:14 +0900

    create delete method in achvm.

--- backend/src/main/java/com/eggmoney/backend/model/service/AchvmService.java
+++ backend/src/main/java/com/eggmoney/backend/model/service/AchvmService.java
@@ -15,4 +15,6 @@ public interface AchvmService {
     // 사용자가 달성한 업적의 id 등록
     void addAchvmId(HashMap<String, Integer> params);
 
+    // 사용자의 업적 획득 정보 삭제 (탈퇴 시)
+    void removeAchvmId(int userSeq);
 }
