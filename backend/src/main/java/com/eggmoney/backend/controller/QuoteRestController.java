7c078fa6f334dcca4bec5a1611bf57666797c963
YOUNG <moxnox63@gmail.com>
2022-11-19 20:50:35 +0900

    quote modified with remaining prob

--- backend/src/main/java/com/eggmoney/backend/controller/QuoteRestController.java
+++ backend/src/main/java/com/eggmoney/backend/controller/QuoteRestController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Random;
-
 @RestController
 @RequestMapping("/api")
 public class QuoteRestController {
@@ -18,9 +16,7 @@ public class QuoteRestController {
     private QuoteService quoteService;
 
     @GetMapping("/quote")
-    public ResponseEntity<String> selectQuote(int id){
-        Random rand = new Random();
-        id = rand.nextInt(20);
-        return new ResponseEntity<String>(quoteService.selectQuote(id), HttpStatus.OK);
+    public ResponseEntity<String> selectQuote(){
+        return new ResponseEntity<String>(quoteService.selectQuote(), HttpStatus.OK);
     }
 }
