8668475406cfc33af8959820d8346e9074c22b41
hiflo <xswaqz@naver.com>
2022-11-20 13:06:15 +0900

    create loginform

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <LoginForm msg="로그인 페이지 구현중입니다."></LoginForm>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import LoginForm from '@/components/LoginForm.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    // HelloWorld
+    LoginForm
   }
 }
 </script>
