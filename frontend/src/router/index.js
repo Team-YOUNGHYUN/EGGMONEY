b729faf668f1d548552a5d57bb3e316e211c2167
YOUNG <moxnox63@gmail.com>
2022-11-22 12:05:37 +0900

    calendar processing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,13 +2,14 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import UserView from "../views/UserView.vue";
-import DiaryView from "../views/DiaryView.vue"
+import DiaryView from "../views/DiaryView.vue";
 import LoginForm from "../components/LoginForm.vue";
 import UserRegist from "../components/user/UserRegist.vue";
 import UserDetail from "../components/user/UserDetail.vue";
 import QuestDetail from "../components/quest/QuestDetail.vue";
 import QuestUpdate from "../components/quest/QuestUpdate.vue";
 import DiaryDetail from "../components/diary/DiaryDetail.vue";
+import CalendarDetail from "../components/diary/CalendarDetail.vue";
 
 Vue.use(VueRouter);
 
@@ -43,8 +44,16 @@ const routes = [
         path: "",
         name: "DiaryDetail",
         component: DiaryDetail,
-      }
-    ]
+      },
+    ],
+  },
+  // Calendar는 메인 페이지와 Diary 페이지에서 각각 보여야 하므로
+  // diary의 child component가 아닌 개별 component로 빼둠
+  {
+    // 임의로 설정한 calendar 경로
+    path: "/calendar",
+    name: "CalendarDetail",
+    component: CalendarDetail,
   },
   {
     path: "/quest",
