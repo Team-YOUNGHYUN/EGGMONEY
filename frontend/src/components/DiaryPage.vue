459ab4edc0ed95542e9dca4e48288626051cfacc
YOUNG <moxnox63@gmail.com>
2022-11-23 01:04:22 +0900

    calendar in progress

--- frontend/src/components/DiaryPage.vue
+++ frontend/src/components/DiaryPage.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <h1>router: 여기부터 DiaryPage 영역이다!</h1>
+    <calendar-detail />
   </div>
 </template>
 
 <script>
 // import MainCalendar from "../components/diary";
-export default {
+import CalendarDetail from "./diary/CalendarDetail.vue";
 
-}
+export default {
+  components: {
+    CalendarDetail,
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
