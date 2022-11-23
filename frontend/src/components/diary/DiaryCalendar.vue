2422ef845e82996ae2b06a2fd396c1940e5737ae
YOUNG <moxnox63@gmail.com>
2022-11-23 15:59:47 +0900

    moneyshop in progress

--- frontend/src/components/diary/DiaryCalendar.vue
+++ frontend/src/components/diary/DiaryCalendar.vue
@@ -2,19 +2,19 @@
   <div class="text-center section">
     <h2 class="h2">EGG DIARY</h2>
     <vc-calendar
-    @dayclick="selectDate"
+      @dayclick="selectDate"
       color="blue"
       is-range
       :colums="$screens({ default: 1, lg: 2 })"
       class="custom-calendar max-w-full"
       :attributes="attributes"
     />
-  <button @click="test">콘솔</button>
+    <button @click="test">콘솔</button>
   </div>
 </template>
 
 <script>
-import {mapGetters} from 'vuex';
+import { mapGetters } from "vuex";
 export default {
   data() {
     // const today = new Date();
@@ -31,71 +31,64 @@ export default {
       //     highlight: "yellow",
       //     dates: new Date(year, month, date),
       //   },
-
-        // {
-        //   key: 2,
-        //   dot: true,
-        //   dates: new Date(year, month, 2),
-        // },
-
-        // {
-        //   key: 3,
-        //   highlight: {
-        //     color: "yellow",
-        //     fillmode: "outline",
-        //   },
-        //   dates: new Date(year, month, 5),
-        // },
-
-        // {
-        // start ~ end 구간 표시
-        //   key: 4,
-        //   highlight: {
-        //     start: { fillMode: "outline" },
-        //     base: { fillMode: "light" },
-        //     end: { fillMode: "outline" },
-        //   },
-        //   dates: {
-        //     start: new Date(year, month + 1, 14),
-        //     end: new Date(year, month + 1, 18),
-        //   },
-        // },
-
-        // {
-        //   // dates에 diary가 작성된 날짜를 저장하고 표시하는 것 까지는 구현 가능
-        //   // 날짜와 diary를 link하는건 고민
-        //   key: 5,
-        //   highlight: {
-        //     style: {
-        //       backgroundColor: "yellow",
-        //     },
-        //   },
-        //   // 여기 dates에 필요한 내용을 insert
-        //   dates: [new Date(year, month, 7), new Date(year, month, 2)],
-        // },
+      // {
+      //   key: 2,
+      //   dot: true,
+      //   dates: new Date(year, month, 2),
+      // },
+      // {
+      //   key: 3,
+      //   highlight: {
+      //     color: "yellow",
+      //     fillmode: "outline",
+      //   },
+      //   dates: new Date(year, month, 5),
+      // },
+      // {
+      // start ~ end 구간 표시
+      //   key: 4,
+      //   highlight: {
+      //     start: { fillMode: "outline" },
+      //     base: { fillMode: "light" },
+      //     end: { fillMode: "outline" },
+      //   },
+      //   dates: {
+      //     start: new Date(year, month + 1, 14),
+      //     end: new Date(year, month + 1, 18),
+      //   },
+      // },
+      // {
+      //   // dates에 diary가 작성된 날짜를 저장하고 표시하는 것 까지는 구현 가능
+      //   // 날짜와 diary를 link하는건 고민
+      //   key: 5,
+      //   highlight: {
+      //     style: {
+      //       backgroundColor: "yellow",
+      //     },
+      //   },
+      //   // 여기 dates에 필요한 내용을 insert
+      //   dates: [new Date(year, month, 7), new Date(year, month, 2)],
+      // },
       // ],
     };
   },
-  computed:{
-    ...mapGetters(["getSelectedDate","getRecords"]),
+  computed: {
+    ...mapGetters(["getSelectedDate", "getRecords"]),
   },
-  methods:{
-    test(){
+  methods: {
+    test() {
       console.log(this.getSelectedDate);
-      
     },
-    async selectDate(date){
+    async selectDate(date) {
       await this.$store.dispatch("setSelectedDate", date.id);
       let params = {
         records: this.getRecords,
         date: this.getSelectedDate,
-      }
+      };
       this.$store.dispatch("setDayRecords", params);
-    }
+    },
   },
-  created(){
-    
-  }
+  created() {},
 };
 </script>
 
